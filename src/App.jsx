a84761aa7eb093925e6dc73aa20ca65177fae901
Joon <dwg4245@gmail.com>
2023-04-01 14:33:25 +0900

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Post from './components/Post'; // 확장자를 붙이지 않는다. 
+
+function App() {
+  return <Post />
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,8 @@
+function Post() {
+  return (<div>
+    <p>Joon</p>
+    <p>React.js is awesome!</p>
+  </div>);
+} // 이렇게 함수의 첫 글자는 대문자를 사용 그리고 함수 안에서는 JSX 코드를 반환해야 한다. 
+
+export default Post;
