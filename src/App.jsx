54e63cf7c942716486bcad64611691122ce2e361
Joon <dwg4245@gmail.com>
2023-05-08 21:49:35 +0900

    ~491

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import Post from './components/Post'; // 확장자를 붙이지 않는다. 
+import Post from "./components/Post"; // 확장자를 붙이지 않는다.
 
 function App() {
-  return <Post />
+  return (
+    <main>
+      <Post author="Maximilian" body="React.js is awesome" />
+      <Post author="Manuel" body="Check out the full course!" />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,12 @@
-function Post() {
-  return (<div>
-    <p>Joon</p>
-    <p>React.js is awesome!</p>
-  </div>);
-} // 이렇게 함수의 첫 글자는 대문자를 사용 그리고 함수 안에서는 JSX 코드를 반환해야 한다. 
+import classes from "./Post.module.css";
+
+function Post(props) {
+  return (
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </div>
+  );
+} // 이렇게 함수의 첫 글자는 대문자를 사용 그리고 함수 안에서는 JSX 코드를 반환해야 한다.
 
 export default Post;
