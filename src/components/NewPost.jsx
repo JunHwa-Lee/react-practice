35a0a1261cc9e4863477d76b3d0e57bfbc73f102
Joon <dwg4245@gmail.com>
2023-05-13 21:34:19 +0900

    496 리액트에서 한 상태를 다른 컴포넌트와 연결해 주기

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,24 +1,15 @@
-import { useState } from "react";
-
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [enteredBody, setEnteredBody] = useState("");
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  } // 함수는 내가 만든 이벤트가 발생하면 뭘 해야 할지 리액트에게 알려준다.
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   ); // className과 마찬가지로 htmlFor도 기존과 다르다. 기존은 그냥 for 이었지만 변형되었다. 이제 큰 변형들은 다 배웠다.
