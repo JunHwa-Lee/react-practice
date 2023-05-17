9ef563903d29d2a09e85900757fa44f0a1050093
JunHwa-Lee <dwg4245@naver.com>
2023-05-17 21:49:45 +0900

    497 컴포넌트로 다른 컴포넌트 감싸기

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,10 @@
 import classes from "./Post.module.css";
 
-function Post(props) {
+function Post({ author, body }) {
   return (
     <li className={classes.post}>
-      <p className={classes.author}>{props.author}</p>
-      <p className={classes.text}>{props.body}</p>
+      <p className={classes.author}>{author}</p>
+      <p className={classes.text}>{body}</p>
     </li>
   );
 } // 이렇게 함수의 첫 글자는 대문자를 사용 그리고 함수 안에서는 JSX 코드를 반환해야 한다.
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
@@ -17,10 +18,12 @@ function PostsList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
